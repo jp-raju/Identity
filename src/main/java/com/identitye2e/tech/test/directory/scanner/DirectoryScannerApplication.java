264a1956fc7ab20fb0fd7183dbf15f4cbc332aeb
Jay Raju <jayaprakash.raju@ext.soprasteria.com>
2018-03-03 16:46:55 +0000

    projets merged

---
+++ src/main/java/com/identitye2e/tech/test/directory/scanner/DirectoryScannerApplication.java
@@ -0,0 +1,12 @@
+package com.identitye2e.tech.test.directory.scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectoryScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectoryScannerApplication.class, args);
+	}
+}
